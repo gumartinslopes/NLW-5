dbef4b938f4ebbe4f95ec0b70fcbc13439a78320
Gustavo Martins <gusluud@gmail.com>
2021-04-22 15:20:31 -0300

    update day 3

--- ProjetoNLW/src/routes.ts
+++ ProjetoNLW/src/routes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
+import { MessagesController } from "./controlers/MessagesController";
 import { SettingsController } from "./controlers/SettingsController";
+import { UsersController } from "./controlers/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 routes.post("/settings", settingsController.create);
 
+routes.post("/users", usersController.create);
 
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
 export{ routes };
